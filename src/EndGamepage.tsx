4d1bc75cfa1553ecf987ca483fc7cb1f12bd0b3a
lukasviktorgunnarsson-web <lukasviktorgunnarsson@gmail.com>
2026-06-17 23:23:48 +0200

    new image added

--- src/EndGamepage.tsx
+++ src/EndGamepage.tsx
@@ -4,7 +4,7 @@ const EndGamepage = () => {
     return (
         <div>
             <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-b bg-cover bg-no-repeat from-gray-900 to-black text-center p-6 space-y-8"
-                style={{ backgroundImage: "url('/bg-all-rooms.png')" }}>
+                style={{ backgroundImage: "url('/endgame.png')" }}>
                 <div className="bg-black/70 p-8 rounded-lg shadow-lg max-w-md mx-auto">
                     <h1 className="text-4xl font-bold text-white">Congratulations!</h1>
                     <p className="text-lg text-gray-300 mt-4">
